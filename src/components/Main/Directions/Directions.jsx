f36e5edfc2ffe10160e232e5850a53ee56dbe986
Andrey <griko.and@gmail.com>
2024-04-10 12:40:34 +0200

    "tune directions button&cards"

--- src/components/Main/Directions/Directions.jsx
+++ src/components/Main/Directions/Directions.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { Card } from '../../ui/Card';
 import s from './style.module.scss';
 
 export const Directions = () => {
@@ -13,18 +12,18 @@ export const Directions = () => {
           <span className={s.highlight}>ВПЕЧАТЛЕНИЯ</span>
         </h2>
         <p className={s.description}>
-      Подари своим желания полную свободу. Вы ограничены только собственной фантазией.
+          Подари своим желания полную свободу. Вы ограничены только собственной фантазией.
           <br/>
           <br/>
-      Наша компания имеет безграничные возможности!
+          Наша компания имеет безграничные возможности!
         </p>
+        <button className={s.applyButton}></button>
       </div>
       <div className={s.Cards}>
         <img className={s.card1} src='{..}' alt="Глен-каньон" />
         <img className={s.card2} src='{..}' alt="Выходные на Бали" />
         <img className={s.card3} src='{..}' alt="о. Борнео" />
       </div>
-        <button className={s.applyButton}>Оставить заявку</button>
     </div>
   );
 }
