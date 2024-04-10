067df8a24cca7f266cc53fcb67d500681dba216a
dnszhkv <den1szhuk0v@yandex.ru>
2024-04-10 23:46:20 +0700

    summary

--- src/components/Main/Order/Order.jsx
+++ src/components/Main/Order/Order.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import cn from 'classnames';
 import s from './style.module.scss';
 import { Checkbox } from '../../ui/Checkboxes/Checkbox';
-// import { Button } from '../../ui/Button/Button';
-
 
 export const Order = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    fam: "",
+    email: "",
+    phone: ""
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://127.0.0.1:8000/submitData/", formData);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className={s.order}>
       <div className={s.order__text}>
@@ -22,22 +42,48 @@ export const Order = () => {
       </div>
 
       <div className={s.order__inputs}>
-        <div className={s.order__inputs_row}>
-          <input type="text" placeholder="Имя"/>
-          <input type="text" placeholder="Фамилия"/>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div className={s.order__inputs_row}>
+            <input
+              type="text"
+              placeholder="Имя"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="Фамилия"
+              name="fam"
+              value={formData.fam}
+              onChange={handleChange}
+            />
+          </div>
 
-        <div className={s.order__inputs_row}>
-          <input type="text" placeholder="E-mail"/>
-          <input type="text" placeholder="Телефон"/>
-        </div>
+          <div className={s.order__inputs_row}>
+            <input
+              type="email"
+              placeholder="E-mail"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+            <input
+              type="tel"
+              placeholder="Телефон"
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+            />
+          </div>
 
-        <div className={s.checkbox}>
-          <Checkbox/>
-        </div>
+          <div className={s.checkbox}>
+            <Checkbox />
+          </div>
 
-        {/* <Button/> */}
-      </div>
+          <button type="submit">Оставить заявку</button>
+        </form>
+      </div>      
     </div>
-  )
-}
+  );
+};
