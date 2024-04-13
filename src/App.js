02ba5011bbdf925083a60d4d910de2339609e02a
Veronika <xafling@mail.ru>
2024-04-13 09:24:25 +0300

    fix routing (link to Africa)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import {Routes, Route} from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import s from './app.module.scss';
+import ContinentAfrica from './pages/ContinentAfrica/ContinentAfrica';
 
 const App = () => {
   return (
     <div className={s.app}>
-      <HomePage/>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path="/africa" element={<ContinentAfrica/>}/>
+      </Routes>
+
     </div>
   );
 }
