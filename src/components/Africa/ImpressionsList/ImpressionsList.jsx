7920209615e360c16e92fef51d15b7947ee8ab0e
Aliaksei Yafremau <447654972g@gmail.com>
2024-04-14 11:52:20 +0200

    Correct ImpressionList style

--- src/components/Africa/ImpressionsList/ImpressionsList.jsx
+++ src/components/Africa/ImpressionsList/ImpressionsList.jsx
@@ -8,14 +8,12 @@ export const ImpressionsList = () => {
             <img className={s.image} src={lion} alt="lion" />
             <div className={s.text}>
                 <div className={s.symbol}>“</div>
-                <div className={s.impression}>
-                    <p>lorem ipsum dolor sit<br></br>
+                    <p className={s.impression}>lorem ipsum dolor sit<br></br>
                     amet consectetur. Tortor<br></br>
                     vitae tincidunt nibh<br></br>
                     feugiat pulvinar viverra<br></br>
                     feugiat. Lectus interdum<br></br>
                     eu rutrum nibh feugiat id.</p>
-                </div>
                 <div className={s.signature}>Автор сообщения</div>
             </div>
         </div>
