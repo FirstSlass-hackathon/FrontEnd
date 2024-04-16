1acb4529961c18cf904c2989fade79e6dcc773b2
Andrey <griko.and@gmail.com>
2024-04-16 17:00:51 +0200

    tune amazing & turnoff founder

--- src/components/Africa/AmazingAfrica/Amazing.jsx
+++ src/components/Africa/AmazingAfrica/Amazing.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './style.module.scss';
 import poster from '../../../assets/images/africa_countries/amazingAfrica_1.jpg';
 import element1 from '../../../assets/images/africa_countries/amazingAfrica_2.jpg';
@@ -7,8 +7,14 @@ import element3 from '../../../assets/images/africa_countries/amazingAfrica_4.jp
 import element4 from '../../../assets/images/africa_countries/amazingAfrica_5.jpg';
 
 export const Amazing = () => {
+  const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e) => {
+    setCursorPos({ x: e.clientX, y: e.clientY });
+  };
+
   return (
-    <div className={s.container}>
+    <div className={s.container} onMouseMove={handleMouseMove}>
       <div className={s.amazing}>
         <div className={s.amazing__title}>
           <span className={s.amazing__text}>такая удивительная Африка</span>
@@ -16,16 +22,27 @@ export const Amazing = () => {
         </div>
         <div className={s.amazing__photo}>
           <div className={s.amazing__poster}>
-            <img src={poster} alt={'photo1'}/>
+            <div className={s.zoomContainer}>
+              <img src={poster} alt={'photo1'} className={s.zoom} />
+            </div>
           </div>
           <div className={s.amazing__block}>
-            <img src={element1} alt={'photo2'}/>
-            <img src={element2} alt={'photo3'}/>
-            <img src={element3} alt={'photo4'}/>
-            <img src={element4} alt={'photo5'}/>
+            <div className={s.zoomContainer}>
+              <img src={element1} alt={'photo2'} className={s.zoom} />
+            </div>
+            <div className={s.zoomContainer}>
+              <img src={element2} alt={'photo3'} className={s.zoom} />
+            </div>
+            <div className={s.zoomContainer}>
+              <img src={element3} alt={'photo4'} className={s.zoom} />
+            </div>
+            <div className={s.zoomContainer}>
+              <img src={element4} alt={'photo5'} className={s.zoom} />
+            </div>
           </div>
         </div>
       </div>
+      <div className={s.customCursor} style={{ left: cursorPos.x, top: cursorPos.y }}></div>
     </div>
   )
 }
