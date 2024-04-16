6462aa3e7a207db9fe6ae57cf672ed6d97e3e0e5
Aliaksei Yafremau <447654972g@gmail.com>
2024-04-16 20:02:59 +0200

    Change color of cursor and AboutAfrica

--- src/components/Africa/AboutAfrica/AboutAfrica.jsx
+++ src/components/Africa/AboutAfrica/AboutAfrica.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import s from './style.module.scss';
 import Button from '../../ui/Button/Button';
+import image1 from '../../../assets/images/africa_countries/AboutAfrica_pyramid.jpg'
+import image2 from '../../../assets/images/africa_countries/AboutAfrica_man.jpg'
+import image3 from '../../../assets/images/africa_countries/AboutAfrica_looking_woman.jpg'
+import image4 from '../../../assets/images/africa_countries/AboutAfrica_trees.jpg'
 // import cardAdvantages from '../../ui/Cards/CardAdv';
 // import {scrollToOrder} from '../Main';
 
@@ -8,37 +12,41 @@ export const AboutAfrica = () => {
 
   return (
     <div className={s.aboutAfrica}>
-      <div className={s.imagesContainer}>
-        {/*<img className={s.image1} src={first} alt="пума" />
-        <img className={s.image2} src={second} alt="дерево" />
-  <img className={s.image3} src={third} alt="жирафы" />*/}
+      <div className={s.firstColumn}> {/* Первый ряд */}
+        <div className={s.pyramidImage}>
+          <img src={image1} alt={'photo1'} />
+        </div>
+        <div className={s.description}>
+          <p>Африка&nbsp;&mdash; это загадочный континент, наполненный дикой
+          красотой и&nbsp;удивительными приключениями.
+          </p>
+          <p>
+          От&nbsp;бескрайних пустынь
+          до&nbsp;густых джунглей, от&nbsp;величественных гор до&nbsp;бескрайних
+          пляжей&nbsp;&mdash; здесь каждый найдет что-то по&nbsp;своему вкусу.
+          </p>
+        </div>
+          
+          {/*<Button className={s.button} onClick={scrollToOrder} text={'Оставить заявку'} colorScheme={'black'}/>*/}
       </div>
-      <div className={s.text}>
-        <h2 className={s.title}>Африка</h2>
-        <h1 className={s.name}>
-          <span className={s.highlight}>УНИКАЛЬНЫЕ</span>
-          <br/>
-          <span>ТУРЫ ПО</span>
-          <br/>
-          <span>ДАЛЬНИМ</span>
-          <br/>
-          <span>УГОЛКАМ АФРИКИ</span>
-        </h1>
-        <p className={s.description}>
-        Африка&nbsp;&mdash; это загадочный континент, наполненный дикой
-        красотой и&nbsp;удивительными приключениями. От&nbsp;бескрайних пустынь
-        до&nbsp;густых джунглей, от&nbsp;величественных гор до&nbsp;бескрайних
-        пляжей&nbsp;&mdash; здесь каждый найдет что-то по&nbsp;своему вкусу.
-          <br/>
-          <br/>
-          Путешествие по&nbsp;Африке обещает незабываемые впечатления
-          и&nbsp;возможность познакомиться с&nbsp;удивительными культурами
-          и&nbsp;традициями местных жителей. Вы&nbsp;сможете отправиться
-          на&nbsp;сафари и&nbsp;увидеть в&nbsp;дикой природе слонов, львов,
-          жирафов и&nbsp;других животных, либо отдохнуть на&nbsp;роскошных
-          белоснежных пляжах у&nbsp;бирюзового океана.
-        </p>
-        <Button className={s.button} /*onClick={scrollToOrder}*/ text={'Оставить заявку'} colorScheme={'black'}/>
+      <div className={s.secondColumn}> {/* Второй ряд */}
+        <img className={s.manImage} src={image2} alt={'photo2'} />
+        <img className={s.womanImage}src={image3} alt={'photo3'} />
+      </div>
+      <div className={s.text}> {/* Третий ряд */}
+        <div>
+          <h2 className={s.title}>Африка</h2>
+          <h1 className={s.name}>
+            <span className={s.highlight}>УНИКАЛЬНЫЕ</span>
+            <br/>
+            <span>ТУРЫ ПО</span>
+            <br/>
+            <span>ДАЛЬНИМ</span>
+            <br/>
+            <span>УГОЛКАМ АФРИКИ</span>
+          </h1>
+        </div>
+        <img className={s.treesImage} src={image4} alt={'photo4'} />
       </div>
     </div>
   );
