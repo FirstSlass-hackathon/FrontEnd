e219c597201655da8ad817a1c21728b26564f092
Andrey <griko.and@gmail.com>
2024-04-16 09:37:39 +0200

    start block2/ not finish & tune block4 without sizes

--- src/components/Africa/ImpressionsList/ImpressionsList.jsx
+++ src/components/Africa/ImpressionsList/ImpressionsList.jsx
@@ -1,39 +1,45 @@
 import React, { useState } from "react";
 import s from './style.module.scss';
 import lion from '../../../assets/images/lion.jpeg';
+//import { CSSTransition } from 'react-transition-group';
+
+const review1 = 'First Class - это просто находка для туриста, которого уже сложно удивить. Компания организует такие туры, которые невозможно даже себе представить.'
+const review2 = 'Чуть позже здесь будет добавлен еще один отзыв о поездке через Первый класс'
+const review3 = 'Чуть позже здесь будет добавлен еще один отличный отзыв о поездке через Первый класс'
+const author1 = 'КРИСТИНА, МОСКВА'
+const author2 = 'АЛЕКСАНДР, САНКТ-ПЕТЕРБУРГ'
+const author3 = 'КИРИЛЛ, НИЖНИЙ НОВГОРОД'
 
 const impressionsList = [
-  { name: 'imp1', image: lion, text: 'Отзыв 1', author: 'Автор 1' },
-  { name: 'imp2', image: lion, text: 'Отзыв 2', author: 'Автор 2' },
-  { name: 'imp3', image: lion, text: 'Отзыв 3', author: 'Автор 3' }, // Использование импортированного изображения
+  { name: 'imp1', image: lion, text: review1, author: author1 },
+  { name: 'imp2', image: lion, text: review2, author: author2 },
+  { name: 'imp3', image: lion, text: review3, author: author3 }, // Использование импортированного изображения
 ];
 
 export const ImpressionsList = () => {
   const [current, setCurrent] = useState(0);
+  const [fadeOut, setFadeOut] = useState(false);
 
   const nextSlide = () => {
-    setCurrent(current === impressionsList.length - 1 ? 0 : current + 1);
-  };
-
-  const prevSlide = () => {
-    setCurrent(current === 0 ? impressionsList.length - 1 : current - 1);
+    setFadeOut(true);
+    setTimeout(() => {
+      setCurrent(current === impressionsList.length - 1 ? 0 : current + 1);
+      setFadeOut(false);
+    }, 500); // Задержка должна быть равна длительности анимации в CSS
   };
 
   return (
     <div className={s.impressionsList}>
-      <img src={impressionsList[current].image} className={s.image} alt={impressionsList[current].name} />
+      <img src={impressionsList[current].image} className={`${s.image} ${fadeOut ? s.fadeOut : ''}`} alt={impressionsList[current].name} />
       <div className={s.text}>
-        <div className={s.symbol}>“</div>
-        <p className={s.impression}>{impressionsList[current].text}</p>
-        <div className={s.signature}>{impressionsList[current].author}</div>
-        <div className={s.controls}>
-          <button className={`${s.controlButton} ${s.left}`} onClick={prevSlide}>
-            {"\u2190"} {/* Unicode символ для левой стрелки */}
-          </button>
-          <button className={`${s.controlButton} ${s.right}`} onClick={nextSlide}>
-            {"\u2192"} {/* Unicode символ для правой стрелки */}
-          </button>
-        </div>
+        <div className={s.symbol}>"</div>
+        <p className={`${s.impression} ${fadeOut ? s.fadeOut : ''}`}>{impressionsList[current].text}</p>
+        <div className={`${s.signature} ${fadeOut ? s.fadeOut : ''}`}>{impressionsList[current].author}</div>
+      </div>
+      <div className={s.controls}>
+        <button className={`${s.controlButton} ${s.right}`} onClick={nextSlide}>
+          {"\u2192"} {/* Unicode символ для правой стрелки */}
+        </button>
       </div>
     </div>
   );
