241ba97a379231b3124065210beb99a8ebd85177
Veronika <xafling@mail.ru>
2024-04-16 18:17:32 +0300

    add Cursor

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import {Routes, Route} from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import s from './app.module.scss';
 import ContinentAfrica from './pages/ContinentAfrica/ContinentAfrica';
+import Cursor from './components/ui/Cursor/Cursor';
 
 const App = () => {
   return (
-    <div className={s.app}>
-      <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path="/africa" element={<ContinentAfrica/>}/>
-      </Routes>
+    <>
+      <Cursor/>
+      <div className={s.app}>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path="/africa" element={<ContinentAfrica/>}/>
+        </Routes>
+      </div>
+    </>
 
-    </div>
   );
 }
 
