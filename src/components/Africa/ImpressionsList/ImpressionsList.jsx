8c2aa0c9c7911c6be6563b7de100b0dfb89fc918
Andrey <griko.and@gmail.com>
2024-04-17 20:17:07 +0200

    changing africa impression with types

--- src/components/Africa/ImpressionsList/ImpressionsList.jsx
+++ src/components/Africa/ImpressionsList/ImpressionsList.jsx
@@ -1,19 +1,22 @@
 import React, { useState } from "react";
-import s from './style.module.scss';
-import lion from '../../../assets/images/lion.jpeg';
-//import { CSSTransition } from 'react-transition-group';
+import s from './style.module.scss'; // Подключаем стили
+import lion from '../../../assets/images/lion.jpg'; // Импортируем изображения
+import sunset from '../../../assets/images/sunset.jpg';
+import man from '../../../assets/images/man.jpg';
+import icon from '../../../assets/images/“.png';
 
-const review1 = 'First Class - это просто находка для туриста, которого уже сложно удивить. Компания организует такие туры, которые невозможно даже себе представить.'
-const review2 = 'Чуть позже здесь будет добавлен еще один отзыв о поездке через Первый класс'
-const review3 = 'Чуть позже здесь будет добавлен еще один отличный отзыв о поездке через Первый класс'
-const author1 = 'КРИСТИНА, МОСКВА'
-const author2 = 'АЛЕКСАНДР, САНКТ-ПЕТЕРБУРГ'
-const author3 = 'КИРИЛЛ, НИЖНИЙ НОВГОРОД'
+const review1 = 'First Class - это просто находка для туриста, которого уже сложно удивить. Компания организует такие туры, которые невозможно даже себе представить.';
+const review2 = 'Мы провели незабываемую неделю в Африке, отдыхая на уединенном острове и наслаждаясь роскошным сервисом. Это было по-настоящему эксклюзивное путешествие, которое мы запомним на всю жизнь!';
+const review3 = 'Отдых в Африке оказался для нас настоящим открытием! Мы побывали в таинственных племенных поселениях, попробовали местную кухню и окунулись в мир традиций и обычаев этого загадочного континента. Настоятельно рекомендую!';
+
+const author1 = 'КРИСТИНА, МОСКВА';
+const author2 = 'АНТОН, САНКТ-ПЕТЕРБУРГ';
+const author3 = 'КИРИЛЛ, МОСКВА';
 
 const impressionsList = [
-  { name: 'imp1', image: lion, text: review1, author: author1 },
-  { name: 'imp2', image: lion, text: review2, author: author2 },
-  { name: 'imp3', image: lion, text: review3, author: author3 }, // Использование импортированного изображения
+  { name: 'imp1', icon: icon, image: lion, text: review1, author: author1 },
+  { name: 'imp2', icon: icon, image: sunset, text: review2, author: author2 },
+  { name: 'imp3', icon: icon, image: man, text: review3, author: author3 },
 ];
 
 export const ImpressionsList = () => {
@@ -29,17 +32,23 @@ export const ImpressionsList = () => {
   };
 
   return (
-    <div className={s.impressionsList}>
-      <img src={impressionsList[current].image} className={`${s.image} ${fadeOut ? s.fadeOut : ''}`} alt={impressionsList[current].name} />
-      <div className={s.text}>
-        <div className={s.symbol}>"</div>
-        <p className={`${s.impression} ${fadeOut ? s.fadeOut : ''}`}>{impressionsList[current].text}</p>
-        <div className={`${s.signature} ${fadeOut ? s.fadeOut : ''}`}>{impressionsList[current].author}</div>
-      </div>
-      <div className={s.controls}>
-        <button className={`${s.controlButton} ${s.right}`} onClick={nextSlide}>
-          {"\u2192"} {/* Unicode символ для правой стрелки */}
-        </button>
+    <div className={s.container1}> {/* Главный блок */}
+      <div className={s.container2}> {/* Блок 2 с отступами */}
+        <div className={s.block3}> {/* Блок 3 с фотографией */}
+          <img src={impressionsList[current].image} className={`${s.image} ${fadeOut ? s.fadeOut : ''}`} alt={impressionsList[current].name} />
+        </div>
+        <div className={s.block4}> {/* Блок 4 с текстом и кнопкой */}
+          <div className={s.text}>
+          <img src={impressionsList[current].icon} className={`${s.symbol} ${fadeOut ? s.fadeOut : ''}`} alt={impressionsList[current].name}/>
+            <p className={`${s.impression} ${fadeOut ? s.fadeOut : ''}`}>{impressionsList[current].text}</p>
+            <div className={`${s.signature} ${fadeOut ? s.fadeOut : ''}`}>{impressionsList[current].author}</div>
+          </div>
+          <div className={s.controls}>
+            <button className={`${s.controlButton} ${s.right}`} onClick={nextSlide}>
+              {"\u2192"} {/* Unicode символ для правой стрелки */}
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
