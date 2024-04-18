ebe0d5f9175388cfc4bc2defc0a50e5951222c8d
Veronika <xafling@mail.ru>
2024-04-18 14:51:36 +0300

    fix Button

--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -3,12 +3,12 @@ import s from './style.module.scss';
 
 export const Button = ({ className, text, onClick, colorScheme, ...props }) => {
   const buttonClassName = colorScheme === 'white' ? s.buttonWhite : s.buttonBlack;
-  const textColor = colorScheme === 'white' ? 'white' : 'black'; // Цвет текста
+  // const textColor = colorScheme === 'white' ? 'white' : 'black'; // Цвет текста
 
   return (
     <button
       className={`${s.button} ${buttonClassName} ${className}`}
-      style={{ color: textColor }} // Применяем цвет текста
+      // style={{ color: textColor }} // Применяем цвет текста
       onClick={onClick}
       {...props}
     >
