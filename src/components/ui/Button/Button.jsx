56cf441d5dd2b26c01d70790a80a32e028722630
MarinaKononenko <Vutsan.m@gmail.com>
2024-04-18 11:39:19 +0300

    changed the background for Order

--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import s from './style.module.scss';
 
-export const Button = ({className, text, onClick, colorScheme, ...props}) => {
+export const Button = ({ className, text, onClick, colorScheme, ...props }) => {
+  const buttonClassName = colorScheme === 'white' ? s.buttonBlack : s.buttonWhite;
+  const textColor = colorScheme === 'white' ? 'white' : 'white'; // Цвет текста
 
-	const buttonClassName = colorScheme === 'black' ? s.buttonBlack : s.buttonWhite;
-
-	return (
-		<button
-		className={`${s.button} ${buttonClassName} ${className}`}
-			onClick={onClick}
-			{...props}>
-			{text}
-		</button>
-	);
+  return (
+    <button
+      className={`${s.button} ${buttonClassName} ${className}`}
+      style={{ color: textColor, height: '100px', width: '400px', fontSize: '25px' }} // Изменяем размеры кнопки и текста
+      onClick={onClick}
+      {...props}
+    >
+      {text}
+    </button>
+  );
 }
 
-export default Button
+export default Button;
