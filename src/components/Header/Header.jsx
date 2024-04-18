a3f77dab9591d6ac0ff288cf94a1ef8024bd1a65
Veronika <xafling@mail.ru>
2024-04-18 14:16:58 +0300

    tune header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { Button } from "../ui/Button/Button";
 import { NavBar } from '../ui/Navbar/navbar';
 import { scrollToOrder } from '../Main/Main';
 import logo from '../../assets/images/logo-3 1.svg';
-// import videoBackground from '../../assets/video/Travel2 comp.mp4'; // Replace 'background-video.mp4' with your video file
 import s from './styles.module.scss';
 
 export const Header = () => {
+
+  const location = useLocation();
+  const [backgroundColor, setBackgroundColor] = useState('');
+
+  // Обновляем цвет бэкграунда в зависимости от текущего пути
+  useEffect(() => {
+
+    if (location.pathname === '/africa') {
+      // setBackgroundColor('var(--gradient-colors)');
+      setBackgroundColor('rgba(72, 57, 42, 0.65)');
+
+    } else {
+      setBackgroundColor('rgba(255, 255, 255, 0.65)');
+    }
+  }, [location.pathname]);
+  
   return (
-    <div className={s.header}>
+    <div className={s.header} style={{ background: backgroundColor }}>
       <div className={s.headerContent}>
         <img src={logo} alt={'logo'}/>
         <NavBar />
@@ -17,5 +34,3 @@ export const Header = () => {
     </div>
   )
 }
-
-// export default Header;
