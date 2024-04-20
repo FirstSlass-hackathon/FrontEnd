4e2fb303da48b54a45a10ec0b7d5a9c15fc622dd
MarinaKononenko <Vutsan.m@gmail.com>
2024-04-20 20:50:47 +0300

    Animation for Directions cards while scrolling

--- src/components/Main/Directions/Directions.jsx
+++ src/components/Main/Directions/Directions.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
-import s from './style.module.scss';
+import React, { useEffect, useRef } from 'react';
+import s from './style.module.scss'; // Импортируем стили
 import cardDirections from '../../ui/Cards/CardDirection';
 
 export const Directions = () => {
+  const cardsRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const cards = cardsRef.current.querySelectorAll(`.${s.card}`);
+
+      cards.forEach((card, index) => {
+        const topPosition = card.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        if (topPosition < windowHeight * 0.75) {
+          card.classList.add(s.slideInRight); // Используем новую анимацию
+        } else {
+          card.classList.remove(s.slideInRight); // Убираем класс анимации
+        }
+      });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Очищаем слушатель события при размонтировании компонента
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <div className={s.directions}>
@@ -10,23 +35,22 @@ export const Directions = () => {
         <div className={s.title}>направления</div>
         <div className={s.name}>
           <span>ЛОВИТЕ</span>
-          <br/>
+          <br />
           <span className={s.highlight}>ВПЕЧАТЛЕНИЯ</span>
         </div>
         <p className={s.description}>
-        Ваши желания ничем не&nbsp;ограничены. Вы&nbsp;ограничены только собственной фантазией.
-          <br/>
-          <br/>
+          Ваши желания ничем не&nbsp;ограничены. Вы&nbsp;ограничены только собственной фантазией.
+          <br />
+          <br />
           Наша компания имеет безграничные возможности!
         </p>
       </div>
-      <div className={s.cards}>
+      <div ref={cardsRef} className={s.cards}>
         {cardDirections.map((card, index) => (
-            <div key={index} className={s.card}>
-              {card}
-            </div>
-          ))}
-      </div>
-    </div>
+          <div key={index} className={`${s.card} ${s.animated}`}>
+            {card}
+          </div>
+        ))}
+      </div>    </div>
   );
-}
+};
