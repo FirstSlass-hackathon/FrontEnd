cfbaad244d47e0186257c05e02c7bec7a8d1c773
MarinaKononenko <Vutsan.m@gmail.com>
2024-04-20 21:06:04 +0300

    Animation for Directions cards

--- src/components/Main/Impressions/Impressions.jsx
+++ src/components/Main/Impressions/Impressions.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import s from './style.module.scss';
 import CardImpression from '../../ui/Cards/CardImpression'; // Исправленный импорт
 
 export const Impressions = () => {
+  const cardsRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const cards = cardsRef.current.querySelectorAll(`.${s.cards} .${s.card}`);
+
+      cards.forEach((card, index) => {
+        const leftPosition = card.getBoundingClientRect().left;
+        const windowWidth = window.innerWidth;
+
+        if (leftPosition < windowWidth * 0.75) {
+          card.classList.add(s.slideInLeft); // Используем новую анимацию
+        } else {
+          card.classList.remove(s.slideInLeft); // Убираем класс анимации
+        }
+      });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Очищаем слушатель события при размонтировании компонента
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div className={s.container}>
       <div className={s.impression}>
@@ -15,8 +41,7 @@ export const Impressions = () => {
               <span>КОТОРЫЕ</span> <span className={s.highlight}>ВДОХНОВЛЯЮТ</span>
             </div>
           </div>
-          <div className={s.cards}>
-            {/* Используйте компонент CardImpression напрямую */}
+          <div ref={cardsRef} className={s.cards}>
             <CardImpression />
           </div>
         </div>
