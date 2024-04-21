72b4ffa33924414f72017cba997b3c1b5f5cfe4e
Aliaksei Yafremau <447654972g@gmail.com>
2024-04-21 15:46:14 +0200

    Replace aboutUs translation

--- src/components/Main/AboutUs/AboutUs.jsx
+++ src/components/Main/AboutUs/AboutUs.jsx
@@ -3,34 +3,32 @@ import React from "react";
 import s from './style.module.scss';
 import kacheliImage from '../../../assets/images/swing.jpg';
 import twix from '../../../assets/images/twix.jpg';
-
+import { useTranslation } from "react-i18next";
 
 export const AboutUs = () => {
+
+  const { t } = useTranslation();
+
   return (
     <div className={s.container}>
       <div className={s.aboutUs}>
         <div className={s.aboutUs__title}> 
-          <div className={s.title}>о компании</div>
+          <div className={s.title}>{t("aboutUs.about_company")}</div>
           <div className={s.name}>
-            <span className={s.highlight}>ЭКСКЛЮЗИВНЫЕ</span> <span>ПОЕЗДКИ</span>
+            <span className={s.highlight}>{t("aboutUs.EXCLUSIVE")}</span> <span>{t("aboutUs.TRIPS")}</span>
             <br/>
-            <span>ИНДИВИДУАЛЬНЫЙ ПОДХОД</span>
+            <span>{t("aboutUs.INDIVIDUAL_APPROACH")}</span>
             <br/>
-            <span>ДЛЯ САМЫХ</span> <span className={s.highlight}>ИСКУШЕННЫХ</span> <span>ТУРИСТОВ</span>
+            <span>{t("aboutUs.FOR_THE_MOST")}</span> <span className={s.highlight}>{t("aboutUs.SOPHISTICATED")}</span> <span>{t("TOURISTS")}</span>
           </div>
         </div>
 
         <div className={s.aboutUs__main}>
           <p className={s.description}>
-          First Class предлагает роскошные и&nbsp;неповторимые путешествия
-    по&nbsp;всему миру. Мы&nbsp;специализируемся на&nbsp;организации индивидуальных туров для самых
-    взыскательных клиентов.
+          {t("aboutUs.descr1")}
             <br/>
             <br/>
-            Мы&nbsp;предлагаем широкий спектр услуг, включая организацию проживания в&nbsp;роскошных
-    отелях, бронирование частных самолетов и&nbsp;яхт, организацию эксклюзивных экскурсий
-    и&nbsp;встреч со&nbsp;знаменитостями, а&nbsp;также оказание персональной помощи
-    во&nbsp;время путешествия.
+            {t("aboutUs.descr2")}
           </p>
           {/* <Button onClick={goToLink} text={'О нас'}/> */}
         </div>
