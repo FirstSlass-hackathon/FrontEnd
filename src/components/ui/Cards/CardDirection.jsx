e79cf9612ae372f98f41e30d1cd87c7627ac13c7
dnszhkv <den1szhuk0v@yandex.ru>
2024-04-21 23:48:50 +0700

    add carddirection translation

--- src/components/ui/Cards/CardDirection.jsx
+++ src/components/ui/Cards/CardDirection.jsx
@@ -4,9 +4,11 @@ import s from './style.module.scss';
 import img1 from '../../../assets/images/homePage_cards/directions_card1.jpg';
 import img2 from '../../../assets/images/homePage_cards/directions_card2.jpg';
 import img3 from '../../../assets/images/homePage_cards/directions_card3.jpg';
+import { useTranslation } from 'react-i18next';
 
 export const CardDirection = ({ title, date, direction, img, description_dir, price, ...props }) => {
   const [isHovered, setIsHovered] = useState(false);
+  const { t } = useTranslation();
 
   return (
     <div
@@ -16,13 +18,13 @@ export const CardDirection = ({ title, date, direction, img, description_dir, pr
     >
       <img src={img} className={s.img} alt={"img"} />
       <div className={s.info}>
-        <div className={s.info__title}>{title}</div>
+        <div className={s.info__title}>{t(title)}</div>
         <div className={s.info__offer}>
-          <div className={s.info__date}>{date}</div>
-          <div className={s.info__direction}>{direction}</div>
+          <div className={s.info__date}>{t(date)}</div>
+          <div className={s.info__direction}>{t(direction)}</div>
           {isHovered && (
             <div className={s.info__description_dir}>
-              <div className={s.info__description_text}>{description_dir}</div>
+              <div className={s.info__description_text}>{t(description_dir)}</div>
               <div className={s.info__price}>{price}</div>
             </div>
           )}
@@ -34,27 +36,27 @@ export const CardDirection = ({ title, date, direction, img, description_dir, pr
 
 const titles = [
   {
-    title: 'Америка',
-    date: '3-5 дней',
-    direction: 'Озеро Пауэлл Глен-Каньон',
-    description_dir: 'Невероятные каньоны и природные ландшафты, непохожие ни на что на земле. Озеро расположено в национальной зоне отдыха Глен-Каньон на границе Юты и Аризоны.',
-    price: 'От 6575 $',
+    title: 'carddirection.America',
+    date: 'carddirection.date1',
+    direction: 'carddirection.PowellLakeGlenCanyon',
+    description_dir: 'carddirection.PowellLakeGlenCanyonDesc',
+    price: '6575 $',
     img: img1
   },
   {
-    title: 'Индонезия',
-    date: '2-3 дня',
-    direction: 'выходные на бали',
-    description_dir: 'Проведите незабываемые выходные на курортном острове Бали! Здесь вас ждут белоснежные пляжи, теплое море, изысканная кухня и роскошные SPA-процедуры.',
-    price: 'От 6575 $',
+    title: 'carddirection.Indonesia',
+    date: 'carddirection.date2',
+    direction: 'carddirection.weekendInBali',
+    description_dir: 'carddirection.weekendInBaliDesc',
+    price: '6575 $',
     img: img2
   },
   {
-    title: 'Малайзия',
-    date: '7-12 дней',
-    direction: 'куала-лумпур о. борнео',
-    description_dir: 'На острове Борнео вы отдохнете на восхитительных пляжах. Погрузитесь в  завораживающую природу и встретите экзотических животных, таких как орангутанги.',
-    price: 'От 6575 $',
+    title: 'carddirection.Malaysia',
+    date: 'carddirection.date3',
+    direction: 'carddirection.KualaLumpurBorneo',
+    description_dir: 'carddirection.KualaLumpurBorneoDesc',
+    price: '6575 $',
     img: img3
   }
 ];
