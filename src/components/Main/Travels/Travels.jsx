182db35c0e35f382a53d898fb09221ffb118daed
MarinaKononenko <Vutsan.m@gmail.com>
2024-04-23 00:05:52 +0300

    Аnimation for Cards in Travel while scrolling

--- src/components/Main/Travels/Travels.jsx
+++ src/components/Main/Travels/Travels.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import s from './style.module.scss';
 import cardTravels from '../../ui/Cards/CardTravel';
 import { useTranslation } from 'react-i18next';
 
 export const Travels = () => {
+  const cardsRef = useRef(null);
   const { t } = useTranslation();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const cards = cardsRef.current.querySelectorAll(`.${s.card}`);
+
+      cards.forEach((card, index) => {
+        const topPosition = card.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        if (topPosition < windowHeight * 0.75) {
+          card.classList.add(s.slideInUp); // Добавляем новую анимацию
+        } else {
+          card.classList.remove(s.slideInUp); // Убираем класс анимации
+        }
+      });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <div className={s.container}>
@@ -17,7 +41,7 @@ export const Travels = () => {
             <span>{t("travels.TRIP")}</span> <span className={s.highlight}>{t("travels.taste")}</span>
           </div>
         </div>
-        <div className={s.cards}>
+        <div ref={cardsRef} className={s.cards}>
           {cardTravels.map((card, index) => (
               <div key={index} className={s.card}>
                 {card}
